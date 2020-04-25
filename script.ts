4802be821e3f036e5c4e9ea4718b7ffebf13b7f2
brunodiazu <bruno.diaz.u@mail.pucv.cl>
2020-04-25 17:54:39 -0400

    Model

--- script.ts
+++ script.ts
@@ -1,3 +1,43 @@
+module Cell {
+
+    export enum E_StateCell {
+        FREE = 0,
+        BOMBILLA,
+        ILUMINADO
+    }
+    
+    export enum E_BlockCell {
+        SOLID = -1,
+        CERO = 0,
+        UNO,
+        DOS,
+        TRES,
+        CUATRO
+    }
+
+    export class Cell {
+    }
+
+    export class StateCell extends Cell {
+        state_cell: E_StateCell;
+        
+        constructor(n: E_StateCell){
+            super();
+            this.state_cell = n;
+        }
+    }
+
+    export class Block extends Cell{
+        bombillas_vecinas: E_BlockCell;
+
+        constructor(n: E_BlockCell){
+            super();
+            this.bombillas_vecinas = n;
+        }
+    }
+
+}
+
 class Tablero {
     private grid = document.getElementById("render-grid-id");
     private grid_section: any;
@@ -6,6 +46,7 @@ class Tablero {
     private col:number;
 
     dim:number;
+    private celdas: Cell.Cell[][];
 
     constructor(row: number, col: number) {
         this.grid.innerHTML = "";
@@ -42,10 +83,12 @@ class Tablero {
     render_grid_fill(){
         const container = document.getElementById("render-grid-section");
 
-        var i: number
-        for (i=0; i< this.row * this.col ;i++){
-            let render_template = `
-                            <div id="cell_${i}" class="cell_item" style=
+        var i: number;
+        var j: number;
+        for (i=0; i< this.dim ; i++){
+            for(j=0; j< this.dim ; j++) {
+                let render_template = `
+                            <div id="cell_${i}_${j}" class="cell_item" style=
                                                                         "   border: 1px orange solid;
                                                                             margin: 2px;
                                                                             text-align:center;
@@ -56,23 +99,28 @@ class Tablero {
                             </div>
             `
             container.innerHTML += render_template;
+            }
         }
     }
 
-    change_status_cell(i_cell: number){
+    change_status_cell(i_cell: number, j_cell: number){
 
-        document.getElementById(`cell_${i_cell}`).addEventListener('click', (e) => {
-            let cell = document.getElementById(`cell_${i_cell}`);
-
+        document.getElementById(`cell_${i_cell}_${j_cell}`).addEventListener('click', (e) => {
+            let cell = document.getElementById(`cell_${i_cell}_${j_cell}`);
             switch(cell.innerHTML) {
-                case "0":
-                    cell.innerHTML = "X";
+                case "F":
+                    cell.innerHTML = "-1";
                     cell.style.backgroundColor = "black";
                     cell.style.color = "black";
                     break;
-
-                case "X":
+                case "-1":
+                    cell.innerHTML = "0";
+                    cell.style.backgroundColor = "black";
+                    cell.style.color = "white";
+                    break;
+                case "0":
                     cell.innerHTML = "1";
+                    cell.style.backgroundColor = "black";
                     cell.style.color = "white";
                     break;
                 case "1":
@@ -85,18 +133,162 @@ class Tablero {
                     cell.innerHTML = "4";
                     break;
                 case "4":
-                    cell.innerHTML = "0";
+                    cell.innerHTML = "F";
                     cell.style.backgroundColor = "white";
                     cell.style.color = "white";
                     break;
                 default:
-                    cell.innerHTML = "X";
+                    cell.innerHTML = "-1";
                     cell.style.backgroundColor = "black";
                     break;
             }
         });
     }
+
+    model(){
+        this.celdas = [];
+        for(var i: number = 0; i< this.dim; i++){
+            this.celdas[i] = [];
+            for(var j: number = 0;j <this.dim; j++){
+                let cell = document.getElementById(`cell_${i}_${j}`);
+
+                switch(cell.innerHTML) {
+                    case "F":
+                        this.celdas[i][j] = new Cell.StateCell(Cell.E_StateCell.FREE);
+                        break;
+                    case "-1":
+                        this.celdas[i][j] = new Cell.Block(Cell.E_BlockCell.SOLID);
+                        break;
+                    case "0":
+                        this.celdas[i][j] = new Cell.Block(Cell.E_BlockCell.CERO);
+                        break;
+                    case "1":
+                        this.celdas[i][j] = new Cell.Block(Cell.E_BlockCell.UNO);
+                        break;
+                    case "2":
+                        this.celdas[i][j] = new Cell.Block(Cell.E_BlockCell.DOS);
+                        break;
+                    case "3":
+                        this.celdas[i][j] = new Cell.Block(Cell.E_BlockCell.TRES);
+                        break;
+                    case "4":
+                        this.celdas[i][j] = new Cell.Block(Cell.E_BlockCell.CUATRO);
+                        break;
+                    default:
+                        this.celdas[i][j] = new Cell.StateCell(Cell.E_StateCell.FREE);
+                        break;
+                }
+            }
+        }
+        alert('Model Generated!');
+        //alert(`${((this.celdas[0][0]) as Cell.Block).bombillas_vecinas}`)
+        //alert(`Existen: ${this.count_state_cells(Cell.E_StateCell.FREE)} espacios vacios`);
+        //alert(`Existen: ${this.count_block_cells(Cell.E_BlockCell.TRES)} espacios de bloque con 3`);
+    }
+
+    private count_block_cells(blockCelltToCount: Cell.E_BlockCell){
+        var count = 0;
+        switch(blockCelltToCount){
+            case Cell.E_BlockCell.CERO:
+                for(var i: number = 0; i<this.dim; i++){
+                    for(var j: number = 0; j<this.dim; j++){
+                        if(this.celdas[i][j] instanceof Cell.Block){
+                            if( ((this.celdas[i][j]) as Cell.Block).bombillas_vecinas == Cell.E_BlockCell.CERO){
+                                count++;
+                            }
+                        }
+                    }
+                }
+                break;
+            case Cell.E_BlockCell.UNO:
+                for(var i: number = 0; i<this.dim; i++){
+                    for(var j: number = 0; j<this.dim; j++){
+                        if(this.celdas[i][j] instanceof Cell.Block){
+                            if( ((this.celdas[i][j]) as Cell.Block).bombillas_vecinas == Cell.E_BlockCell.UNO){
+                                count++;
+                            }
+                        }
+                    }
+                }
+                break;
+            case Cell.E_BlockCell.DOS:
+                for(var i: number = 0; i<this.dim; i++){
+                    for(var j: number = 0; j<this.dim; j++){
+                        if(this.celdas[i][j] instanceof Cell.Block){
+                            if( ((this.celdas[i][j]) as Cell.Block).bombillas_vecinas == Cell.E_BlockCell.DOS){
+                                count++;
+                            }
+                        }
+                    }
+                }
+                break;
+            case Cell.E_BlockCell.TRES:
+                for(var i: number = 0; i<this.dim; i++){
+                    for(var j: number = 0; j<this.dim; j++){
+                        if(this.celdas[i][j] instanceof Cell.Block){
+                            if( ((this.celdas[i][j]) as Cell.Block).bombillas_vecinas == Cell.E_BlockCell.TRES){
+                                count++;
+                            }
+                        }
+                    }
+                }
+                break;
+            case Cell.E_BlockCell.CUATRO:
+                for(var i: number = 0; i<this.dim; i++){
+                    for(var j: number = 0; j<this.dim; j++){
+                        if(this.celdas[i][j] instanceof Cell.Block){
+                            if( ((this.celdas[i][j]) as Cell.Block).bombillas_vecinas == Cell.E_BlockCell.CUATRO){
+                                count++;
+                            }
+                        }
+                    }
+                }
+                break;
+        }
+        return count;
+    }
+    private count_state_cells(stateCellToCount: Cell.E_StateCell){
+        var count = 0;
+        switch(stateCellToCount){
+            case Cell.E_StateCell.BOMBILLA:
+                for(var i: number = 0; i<this.dim; i++){
+                    for(var j: number = 0; j<this.dim; j++){
+                        if(this.celdas[i][j] instanceof Cell.StateCell){
+                            if( ((this.celdas[i][j]) as Cell.StateCell).state_cell == Cell.E_StateCell.BOMBILLA){
+                                count++;
+                            }
+                        }
+                    }
+                }
+                break;
+            case Cell.E_StateCell.FREE:
+                for(var i: number = 0; i<this.dim; i++){
+                    for(var j: number = 0; j<this.dim; j++){
+                        if(this.celdas[i][j] instanceof Cell.StateCell){
+                            if( ((this.celdas[i][j]) as Cell.StateCell).state_cell == Cell.E_StateCell.FREE){
+                                count++;
+                            }
+                        }
+                    }
+                }
+                break;
+            case Cell.E_StateCell.ILUMINADO:
+                for(var i: number = 0; i<this.dim; i++){
+                    for(var j: number = 0; j<this.dim; j++){
+                        if(this.celdas[i][j] instanceof Cell.StateCell){
+                            if( ((this.celdas[i][j]) as Cell.StateCell).state_cell == Cell.E_StateCell.ILUMINADO){
+                                count++;
+                            }
+                        }
+                    }
+                }
+                break;
+        }
+        return count;
+    }
 }
+
+var t: Tablero;
 
 var btnGenerateTable = document.getElementById("button-render");
 btnGenerateTable.addEventListener('click', ()=> {
@@ -104,11 +296,24 @@ btnGenerateTable.addEventListener('click', ()=> {
     let row = +(document.getElementById("row-number") as HTMLInputElement).value;
     let col = +(document.getElementById("column-number") as HTMLInputElement).value;
 
-    let t = new Tablero(row, col);
+    t = new Tablero(row, col);
 
-    var i:number
-    for (i=0; i<t.dim*t.dim;i++){
-        t.change_status_cell(i);
+    var i: number;
+    var j:number;
+
+    for(i=0; i<t.dim; i++) {
+        for(j=0; j<t.dim; j++ ) {
+            t.change_status_cell(i, j);
+        }
     }
-});
+    }
+);
 
+
+
+var btnSolverTable = document.getElementById("button-solver");
+btnSolverTable.addEventListener('click', () => {
+
+    t.model();
+
+});
